2bf0c627440a978c72f0abdcd3938f5679794b15
Md. Sazibur Rahman <36544022+shajiv29@users.noreply.github.com>
2022-01-28 23:00:14 +0600

    add project in git

---
+++ bmet/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BmetConfig(AppConfig):
+    name = 'bmet'
---
+++ pdo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PdoConfig(AppConfig):
+    name = 'pdo'
